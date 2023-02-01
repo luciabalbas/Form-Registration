5ad5baa6e83ec49655c29e6a6bae229c70d6b5d6
luciabalbas <luciabalrub@gmail.com>
2023-02-01 19:49:19 +0100

    Introduction Regex

--- index.js
+++ index.js
@@ -22,6 +22,10 @@ function validateForm(event) {
     }
 
     // Correo
+    //Expresion Regular
+    const emailRegex = new RegExp(/^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/, "gm");
+    let validEmail = emailRegex.test(email);
+   
     if (email.value == "") {
         error(email, "Rellene este campo");
     }
